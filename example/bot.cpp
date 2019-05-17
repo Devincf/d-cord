dcba05144413d0e1263e257f0ec171f4ffe458a6
Devincf <devinc.firat@gmail.com>
2019-05-17 20:20:06 +0200

    finished basic gateway connection

---
+++ example/bot.cpp
@@ -0,0 +1,13 @@
+#include "bot.hpp"
+
+
+int main()
+{
+    std::ifstream t("token.txt");
+    std::string str((std::istreambuf_iterator<char>(t)),
+                 std::istreambuf_iterator<char>());
+
+    std::cout << str << "\n";
+    Example e(str);
+    return 0;
+}
--- example/bot.hpp
+++ example/bot.hpp
@@ -1,21 +1,11 @@
+#include <string>
+#include <fstream>
+#include <streambuf>
+
 #include "dppcord/client/DiscordClient.hpp"
 
 
 class Example : public dppcord::DiscordClient
 {
-    public:
-    Example(const std::string& token):dppcord::DiscordClient(token)
-    {
-
-    }
+    using dppcord::DiscordClient::DiscordClient;
 };
-
-int main()
-{
-    std::ifstream t("token.txt");
-    std::string str((std::istreambuf_iterator<char>(t)),
-                 std::istreambuf_iterator<char>());
-
-    Example e;
-    return 0;
-}
